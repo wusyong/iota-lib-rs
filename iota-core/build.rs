ed4d90f53522c30aefaa4c4716dceb356940da4b
Wu Yu Wei <wusyong9104@gmail.com>
2020-05-07 21:56:55 +0800

    Update build.rs

--- iota-core/build.rs
+++ iota-core/build.rs
@@ -1,5 +1,5 @@
 fn main() {
-    println!("cargo:rustc-link-search=native=/home/wusyong/Desktop/iota.rs/iota-core/src/");
+    println!("cargo:rustc-link-search=native=your_path_to/iota-core/src/");
     println!("cargo:rustc-link-lib=static=keccak");
     println!("cargo:rustc-link-lib=static=common");
 }
